deafb8f6fac5562177618a719a286857cb1811b4
None <None@None.com>
2024-01-11 12:39:01 -0700

    random commit

--- SOURCE CODE/PCP_app/PCP_main.py
+++ SOURCE CODE/PCP_app/PCP_main.py
@@ -1,13 +1,9 @@
 import json, tkinter as tk
 import serial, time
-import encryption, secrets
+import encryption, random
 
-def bt_write(port, data):
-    s = serial.Serial(port, 9600,timeout=10)
-    print("connected!")
-    time.sleep(10)
-    s.write(data+b'\n')
-    print("Sent Message!")
+def split_string_nth(string, n):
+    return [string[i:i+n] for i in range(0, len(string), n)]
 
 def Title(boolean:bool):
     if boolean:
@@ -187,15 +183,20 @@ vitalink_info={
 with open("env_secure.json", 'w+') as outfile:
     json.dump(vitalink_info, outfile, indent=4)
 
+passkey = random.randbytes(32)
+nonce = random.randbytes(8)
 
-enc=encryption.File_Enc()
-passkey=secrets.randbits(256)
-nonce=secrets.randbits(64)
-
+enc = encryption.File_Enc()
 enc.enc("env_secure.json", passkey, nonce)
-with open("env_secure.json.sla.bfs.aes", "wb+") as infile:
-    bt_write('COM5', infile.read())
 
-#send data to server. File used for simplicity.
+s = serial.Serial('COM5', 9600, timeout=10)
+with open("env_secure.json", "rb+") as infile:
+    for line in infile:
+        sections = split_string_nth(line, 7)
+        for section in sections:
+            s.write(section+b'\n')
+            time.sleep(0.1)
+
+# Send data to the server. File used for simplicity.
 with open("../server/server", "wb+") as outfile:
-    outfile.write(passkey+b'\n\n'+nonce)
+    outfile.write(passkey + b'\n\n' + nonce)
